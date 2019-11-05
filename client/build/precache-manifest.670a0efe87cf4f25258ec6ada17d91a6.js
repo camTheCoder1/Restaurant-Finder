0e1dadeda0e5c0a15b2229d0db47c6050fc277d9
Cameron  Morgan <cm09516@georgiasouthern.edu>
2019-11-05 15:41:15 -0500

    initial commit

---
+++ client/build/precache-manifest.670a0efe87cf4f25258ec6ada17d91a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03307b5fae6dc75ccd4bf41417b64473",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b372882fa46a5d9a01b",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "2b372882fa46a5d9a01b",
+    "url": "/static/js/main.4b0fb4c3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
